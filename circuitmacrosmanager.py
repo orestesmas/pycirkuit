2b776bfdca9ec21fde71d642454fcde7235a08d6
Orestes Mas <orestes@tsc.upc.edu>
2018-11-21 18:57:23 +0100

    Embrió de la idea d'un "Circuit macros manager" extreta del cirkuit
    Mirar el codi C++, que és força entenedor.

---
+++ circuitmacrosmanager.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from PyQt5 import QCore
+
+class CircuitMacrosManager(QCore.QObject):
+    """
+    Class documentation goes here.
+    """
+    def __init__(self, parent=None):
+        """
+        Constructor
+
+        @param parent reference to the parent widget
+        @type QWidget
+        """
+        pass
