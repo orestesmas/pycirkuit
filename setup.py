17e20b11031b5c812e79766e636a626127885feb
Orestes Mas <orestes@tsc.upc.edu>
2018-11-23 16:37:16 +0100

    Començat a implementar el follón de les llicències

--- setup.py
+++ setup.py
@@ -1,9 +1,26 @@
+"""
+This file is part of PyCirkuit.
+
+PyCirkuit is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+PyCirkuit is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with PyCirkuit.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
 from setuptools import setup
 
 setup(name='pycirkuit',
       version='0.1',
       description='A front-end for Circuit Macros',
-      url='http://github.com/storborg/funniest',
+      url='https://gitlab.upc.edu/CSL/Programari/pycirkuit.git',
       author='Orestes Mas',
       author_email='orestes@tsc.upc.edu',
       license='GPL',
