799947ccd6e1bb2f02ce281e81c7117a09e0a886
Orestes Mas <orestes@tsc.upc.edu>
2018-11-23 14:34:53 +0100

    Reubicació de fitxers font
    Pas previ per convertir-ho en paquet distribuïble

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pycirkuit',
+      version='0.1',
+      description='A front-end for Circuit Macros',
+      url='http://github.com/storborg/funniest',
+      author='Orestes Mas',
+      author_email='orestes@tsc.upc.edu',
+      license='GPL',
+      packages=['pycirkuit'],
+      zip_safe=False)
