35d592c6a08854dc4548006534f326da17220845
Orestes Mas <orestes@tsc.upc.edu>
2018-11-25 03:13:18 +0100

    Llicència

--- pycirkuit/circuitmacrosmanager.py
+++ pycirkuit/circuitmacrosmanager.py
@@ -2,22 +2,22 @@
 """
 Module implementing a CircuitMacros Manager class.
 """
-"""
-This file is part of PyCirkuit.
-
-PyCirkuit is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-PyCirkuit is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with PyCirkuit.  If not, see <https://www.gnu.org/licenses/>.
-"""
+# Copyright (C) 2018 Orestes Mas
+# This file is part of PyCirkuit.
+#
+# PyCirkuit is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# PyCirkuit is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with PyCirkuit.  If not, see <https://www.gnu.org/licenses/>.
+#
 
 # Standard library imports
 import os, shutil, tarfile
