9c663a70c190a2a154d256bce96a3d8958e3e18b
Orestes Mas <orestes@tsc.upc.edu>
2018-11-30 13:07:12 +0100

    Purged spurious translations from original translation files

--- pycirkuit/L10n/pycirkuit_empty.ts
+++ pycirkuit/L10n/pycirkuit_empty.ts
@@ -1,5 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="2.0" language="ca_ES" sourcelanguage="">
+<!DOCTYPE TS>
+<TS version="2.1" language="ca_ES">
 <context>
     <name>MainWindow</name>
     <message>
@@ -11,47 +12,47 @@
     <message>
         <location filename="../ui/Ui_mainwindow.py" line="169"/>
         <source>PyCirkuit - by Orestes Mas</source>
-        <translation>PyCircuit - per Orestes Mas</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../ui/Ui_mainwindow.py" line="170"/>
         <source>Source Editor</source>
-        <translation>Editor de la font</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../ui/Ui_mainwindow.py" line="171"/>
         <source>Process and display</source>
-        <translation>Processa i mostra</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../ui/Ui_mainwindow.py" line="172"/>
         <source>Export to TIkZ</source>
-        <translation>Exporta a TIkZ</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../ui/Ui_mainwindow.py" line="173"/>
         <source>Fi&amp;le</source>
-        <translation>&amp;Fitxer</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../ui/Ui_mainwindow.py" line="174"/>
         <source>Help</source>
-        <translation>Ajuda</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../ui/Ui_mainwindow.py" line="175"/>
         <source>Setti&amp;ngs</source>
-        <translation>Arra&amp;njaments</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../ui/Ui_mainwindow.py" line="176"/>
         <source>&amp;Preview</source>
-        <translation>&amp;Previsualització</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../ui/Ui_mainwindow.py" line="177"/>
         <source>&amp;Quit</source>
-        <translation>S&amp;urt</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../ui/Ui_mainwindow.py" line="178"/>
@@ -91,37 +92,37 @@
     <message>
         <location filename="../ui/Ui_mainwindow.py" line="185"/>
         <source>&amp;New</source>
-        <translation>&amp;Nou</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../ui/Ui_mainwindow.py" line="186"/>
         <source>New Drawing</source>
-        <translation>Nou dibuix</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../ui/Ui_mainwindow.py" line="187"/>
         <source>Ctrl+N</source>
-        <translation>Ctrl+N</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../ui/Ui_mainwindow.py" line="188"/>
         <source>&amp;Save</source>
-        <translation>&amp;Desa</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../ui/Ui_mainwindow.py" line="189"/>
         <source>Ctrl+S</source>
-        <translation>Ctrl+S</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../ui/Ui_mainwindow.py" line="190"/>
         <source>Sa&amp;ve As...</source>
-        <translation>Desa &amp;com a...</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../ui/Ui_mainwindow.py" line="191"/>
         <source>Ctrl+Shift+S</source>
-        <translation>Ctrl+Shift+S</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -131,13 +132,11 @@
         <source>Cannot find the {toolLongName}!
 
 </source>
-        <translation>No s&apos;ha trobat el {toolLongName}!
-
-</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../mainwindow.py" line="371"/>
-        <source>Please ensure that you have this application properly installed and the executable &#xc2;&#xab;{execName}&#xc2;&#xbb; is in the PATH.
+        <source>Please ensure that you have this application properly installed and the executable Â«{execName}Â» is in the PATH.
 
 </source>
         <translation type="unfinished"></translation>
@@ -145,7 +144,7 @@
     <message>
         <location filename="../mainwindow.py" line="372"/>
         <source>Cannot generate the preview.</source>
-        <translation>No s&apos;ha pogut generar la previsualització.</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../mainwindow.py" line="385"/>
@@ -162,7 +161,7 @@
     <message>
         <location filename="../mainwindow.py" line="387"/>
         <source>Error</source>
-        <translation>Error</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../mainwindow.py" line="402"/>
@@ -183,16 +182,12 @@
 PyCirkuit is a compiler/renderer of circuit diagrams written using the Dwight Aplevich&apos;s &apos;Circuit Macros&apos;.
 Being written in python, the code and ideas are largely based on &apos;cirkuit&apos; C++ program, by Matteo Agostinelli.
 </source>
-        <translation>Copyright (c) 2018 Orestes Mas
-
-El PyCirkuit és un compilador/visualitzador de diagrames circuitals descrits emprant l&apos;eina «Circuit Macros» de Dwight Aplevich.
-És escrit en llenguatge python, però el disseny i les idees es basen en gran mesura en l&apos;aplicació «cirkuit», escrita en C++ per Matteo Agostinelli.
-</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../mainwindow.py" line="250"/>
         <source>About PyCirkuit</source>
-        <translation>Sobre el PyCirkuit</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -200,7 +195,7 @@ El PyCirkuit és un compilador/visualitzador de diagrames circuitals descrits em
     <message>
         <location filename="../mainwindow.py" line="113"/>
         <source>Converting: Circuit Macros -&gt; PIC</source>
-        <translation>Conversió: Circuit Macros -&gt; PIC</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -208,22 +203,22 @@ El PyCirkuit és un compilador/visualitzador de diagrames circuitals descrits em
     <message>
         <location filename="../mainwindow.py" line="357"/>
         <source>&apos;M4&apos; Macro Processor</source>
-        <translation>Processador de Macros «M4»</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../mainwindow.py" line="357"/>
         <source>&apos;PIC&apos; language compiler</source>
-        <translation>Compilador de llenguatge «PIC»</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../mainwindow.py" line="357"/>
         <source>pdfLaTeX program</source>
-        <translation>Programa pdfLaTeX</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../mainwindow.py" line="357"/>
         <source>PDF to PNG image converter</source>
-        <translation>Convertidor d&apos;imatge PDF a PNG</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -231,42 +226,42 @@ El PyCirkuit és un compilador/visualitzador de diagrames circuitals descrits em
     <message>
         <location filename="../ui/Ui_configdialog.py" line="114"/>
         <source>Config Dialog</source>
-        <translation>Diàleg de configuració</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../ui/Ui_configdialog.py" line="118"/>
         <source>General</source>
-        <translation>General</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../ui/Ui_configdialog.py" line="120"/>
         <source>Circuit Macros Location</source>
-        <translation>Ubicació de les Circuit Macros</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../ui/Ui_configdialog.py" line="124"/>
         <source>Enter Path Name</source>
-        <translation>Introduïu la ruta</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../ui/Ui_configdialog.py" line="122"/>
         <source>Choose Dir...</source>
-        <translation>Escolliu directori...</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../ui/Ui_configdialog.py" line="123"/>
         <source>LaTeX Template File</source>
-        <translation>Fitxer de plantilla LaTeX</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../ui/Ui_configdialog.py" line="125"/>
         <source>Choose File...</source>
-        <translation>Escolliu...</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../ui/Ui_configdialog.py" line="126"/>
         <source>RadioB&amp;utton</source>
-        <translation>&amp;dummy--</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
