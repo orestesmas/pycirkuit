0b2d0b62adb6a1f6af86d4ba58ffa6db257b9a1a
Orestes Mas <orestes@tsc.upc.edu>
2018-12-24 01:49:07 +0100

    Started zoomable image class. Work in progress.

---
+++ pycirkuit/imageviewer.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Module implementing pycktImageViewer
+"""
+# Copyright (C) 2018 Orestes Mas
+# This file is part of PyCirkuit.
+#
+# PyCirkuit is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# PyCirkuit is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with PyCirkuit.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+# Standard library imports
+
+
+# Third-party imports
+from PyQt5.QtWidgets import QScrollArea
+
+
+
+class pycktImageViewer(QScrollArea):
+    """
+    Class documentation goes here.
+    """
+    def __init__(self, parent=None):
+        """
+        Constructor
+
+        @param parent reference to the parent widget
+        @type QWidget
+        """
+        super().__init__(parent)
