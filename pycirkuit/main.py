b8df7d5bb19a32de2608c406c2837cf82a077a9b
Orestes Mas <orestes@tsc.upc.edu>
2019-02-09 19:58:38 +0100

    Closes #9 -- Partially fixed
    Changed the "Organization Name" to "PyCirkuit" instead of "UPC", as
    the latter, although correct, won't help a user locate the PyCirkuit
    files on the disk.
    
    I prefer to maintain a non-empty "Organization Name", because lots of
    applications are doing the same, specially those based on Qt.

--- pycirkuit/main.py
+++ pycirkuit/main.py
@@ -41,7 +41,7 @@ def main():
         app.installTranslator(translator)
         
     # These two next values are passed to every instance of QSettings everywhere in the app
-    QCoreApplication.setOrganizationName("UPC")
+    QCoreApplication.setOrganizationName("PyCirkuit")
     QCoreApplication.setApplicationName("pycirkuit")
     my_mainWindow = MainWindow()
     my_mainWindow.show()
